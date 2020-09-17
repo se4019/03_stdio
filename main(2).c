54b130780384df0e29de63dabf1d5c9b60d59ff3
se4019 <70657061+se4019@users.noreply.github.com>
2020-09-17 16:44:09 +0900

    LAST

---
+++ main(2).c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+
+	int a, b;
+	
+	printf("input two integers :");
+	scanf("%d %d" , &a, &b);
+	
+	printf("result is %f/n", (float)a/b);
+
+	return 0;
+}
